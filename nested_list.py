03bc340ec48e04470e063b015f615567558eadc9
TinFe <53035411+TinFe@users.noreply.github.com>
2023-03-08 20:21:12 +0700

    manually create skeleton of nested tree to_do list structure using nested lists

---
+++ nested_list.py
@@ -0,0 +1,11 @@
+# sample: [level, index_of_level, item_name, sub_item_array]
+to = 2
+root = ['_','programming',[]]
+root[to].append([[0],'research',[]])
+root[to].append([[1],'projects',[]])
+root[to].append([[2],'networking',[]])
+root[to][1][to].append([[1,0],'tree to do',[]])
+root[to][0][to].append([[0,0],'read book',[]])
+root[to][2][to].append([[2,0],'go to meetup',[]])
+
+
