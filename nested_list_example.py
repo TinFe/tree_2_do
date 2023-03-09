1347842c599f62a9c3d2e31aeff317a74d250d95
TinFe <53035411+TinFe@users.noreply.github.com>
2023-03-09 19:12:24 +0700

    create get_item and set_item methods, add_item no gives unique paths to new items

--- nested_list_example.py
+++ nested_list_example.py
@@ -9,7 +9,9 @@ root[to][0][to].append([[0,0],'read book',[]])
 root[to][2][to].append([[2,0],'go to meetup',[]])
 root[to][1][to].append([[1,1],'mandelbrot',[]])
 root[to][1][to][0][to].append([[1,0,0],'create tree class using nested lists',[]])
+print(root)
 
+""""
 def print_tree(tree, indent=0):
     for item in tree:
         if isinstance(item, list):
@@ -17,4 +19,6 @@ def print_tree(tree, indent=0):
         else:
             print(" " * 4 * indent + item[1])
 
-print_tree(root)
+            
+"""
+
--- nested_list_object.py
+++ nested_list_object.py
@@ -1,30 +1,43 @@
 class ListTree:
     def __init__(self, root_name) -> None:
         self.root_name = root_name
-        self.root = ['_','programming',[]]
+        self.root = ['root','programming',[]]
         # all items have identical structure to self.root. all items will be inserted into the empty list. Therefore,
         # we need the index of the empty list, as this will be used for inserting other items
         for i in enumerate(self.root):
             if i[1] == []:
                 self.into = i[0]
 
-    def add_item(self, path, item_name):
-        if path == 0:
+    def add_item(self, parent_path, item_name):
+        if parent_path == 'root':
             insert_path = [self.into]
-        else:
-            insert_path = self.convert_insert_path(path)
+            # count direct root children
+            count_children = len(self.root[self.into])
+            new_item_path = [count_children]
 
-        print(insert_path)
-        item = ['_', item_name, []]
+        else:
+            insert_path = self.convert_path(parent_path,'set')
+            count_children = len(self.get_item(self.root, parent_path)[self.into])
+            new_item_path = parent_path
+            new_item_path.append(count_children)
+
+        item = [new_item_path, item_name, []]
         self.root = self.set_item(self.root, insert_path, item)
 
-    def convert_insert_path(self,path:list):
+    def convert_path(self,path:list,set_or_get:str):
         new_path = [self.into]
         for i in path:
             new_path.append(i)
             new_path.append(self.into)
-        return new_path
 
+        if set_or_get == 'set':
+            return new_path
+        elif set_or_get == 'get':
+            new_path.pop()
+            return new_path
+        else:
+            return "INVALID ARGUMENT IN .convert_path(): set_or_get MUST EQUAL 'set' OR 'get'.\n"
+    
     def set_item(self, lst, path, item, depth=0):
         if depth == len(path) - 1:
             lst[path[depth]].append(item)
@@ -32,8 +45,21 @@ class ListTree:
         else:
             self.set_item(lst[path[depth]], path, item, depth+1)
             return lst
+        
+    def get_item(self, lst, path):
+        # convert path 
+        converted_path = self.convert_path(path, 'get')
+
+        def recursive_get(lst, path, depth=0):    
+            if depth == len(path) - 1:
+                return lst[path[depth]]
+            else:
+                return recursive_get(lst[path[depth]], path, depth+1)
+        
+        return recursive_get(lst, converted_path)
+                
 
 # test add_item
 tree = ListTree('programming')
 print(tree.root)
-tree.add_item(0,'research')
+tree.add_item('root','research')
